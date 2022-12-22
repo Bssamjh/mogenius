e897711937810efeacc0c19d6565851b5e975de4
Bssamjh <112725901+Bssamjh@users.noreply.github.com>
2022-12-22 06:48:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://username:token@github.com/username/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://itz-zaid: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@github.com/itz-zaid/mogenius ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 -m telegram &")
